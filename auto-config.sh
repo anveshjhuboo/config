d69144974fbf867120485512be65c213df85abb3
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-04-26 03:16:33 -0400

    Add auto-config.sh for automatic installs

---
+++ auto-config.sh
@@ -0,0 +1,25 @@
+# This file automatically configures the system
+# Install zsh, starship, tmux, neovim, and vscode configs
+
+# Install ZSH and set as default shell
+sudo apt install zsh -y
+chsh -s $(which zsh)
+cp ./zshrc ~/.zshrc
+
+# Install starship (https://starship.rs)
+curl -sS https://starship.rs/install.sh | sh
+echo 'eval "$(starship init bash)"' >> ~/.zshrc
+mkdir -p ~/.config && touch ~/.config/starship.toml
+cp ./starship.toml ~/.config/starship.toml
+
+# Install tmux and copy config
+sudo apt install tmux -y
+mkdir -p ~/config/tmux
+cp ./tmux.conf ~/config/tmux/tmux.conf
+
+# Install neovim and copy config
+sudo apt install neovim -y
+mkdir -p ~/.config/nvim
+cp ./init.vim ~/.config/nvim/init.vim
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Open nvim and run :PlugInstall

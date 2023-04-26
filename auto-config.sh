49dae69baa13eaac910e21ff4eb4366ee7a80f81
Anvesh G. Jhuboo <anvesh.jhuboo@gmail.com>
2023-04-26 03:19:28 -0400

    Update auto-config

--- auto-config.sh
+++ auto-config.sh
@@ -23,3 +23,6 @@ mkdir -p ~/.config/nvim
 cp ./init.vim ~/.config/nvim/init.vim
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 # Open nvim and run :PlugInstall
+
+# Install VSCode
+# Import agj.code-porfile into Files/Preferences/Porfiles/Import Porfile
